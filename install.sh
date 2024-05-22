bf92016f25d65cfdbc671ce206fd8885a33bc6ea
qiitaqiitaykuyo <QiitaQiitaYkuyo@outlook.jp>
2024-05-22 09:37:30 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+### execution: USER;  privilege: sudo ###
 
-CurrentDir=`pwd`
+baseDIR="$(dirname -- $(realpath -- "${BASH_SOURCE[0]:-$0}"))"; echo "$baseDIR"
 
 if (id minecraft) ;then exit 1 ;fi
 if [ -d /opt/MC_Manage ] ;then exit 2 ;fi
@@ -21,28 +22,29 @@ sudo adduser --system \
 
 sudo loginctl enable-linger $New_USER
 
-sudo mkdir -p /opt/MC_Manage/{Properties,World_Backup}
+sudo mkdir -p "/opt/MC_Manage/"{"Properties","World_Backup"}
 sudo chown -R $New_USER:$New_GROUP "/opt/MC_Manage/"
 sudo chmod -R 750 "/opt/MC_Manage/"
 
+sudo chmod -R +x "$baseDIR/Scripts/"*
 
-sudo chmod -R +x ./Scripts/*
-
-bash "./Scripts/USER/minecraft.service"
-bash "./Scripts/USER/start.sh"
-bash "./Scripts/USER/backup.sh"
-bash "./Scripts/USER/stop.sh"
+bash "$baseDIR/Scripts/USER/minecraft.service"
+bash "$baseDIR/Scripts/USER/start.sh"
+bash "$baseDIR/Scripts/USER/backup.sh"
+bash "$baseDIR/Scripts/USER/stop.sh"
 
 sudo chown $New_USER:$New_GROUP "/opt/MC_Manage/start.sh" "/opt/MC_Manage/backup.sh" "/opt/MC_Manage/stop.sh"
 sudo chmod 751 "/opt/MC_Manage/start.sh" "/opt/MC_Manage/backup.sh" "/opt/MC_Manage/stop.sh"
 
+bash "$baseDIR/Scripts/USER/.bash_aliases"
+bash "$baseDIR/Scripts/USER/.bashrc"
 
-bash "./Scripts/USER/.bash_aliases"
-bash "./Scripts/USER/.bashrc"
-
-
-sudo cp -rf "$CurrentDir/Scripts/Minecraft" "/opt/MC_Manage/Scripts"
+sudo \cp -vfRT "$baseDIR/Scripts/Minecraft/" "/opt/MC_Manage/Scripts/"
+sudo bash -c 'for file in `find "/opt/MC_Manage/Scripts/" -maxdepth 1 -type f` ;do sed -i -e "s/\r//g" "$file" && echo $[++i].OK ;done'
 sudo chown -R $New_USER:$New_GROUP "/opt/MC_Manage/Scripts/"
+
+
+### execution: minecraft;  privilege: minecraft ###
 
 sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/Properties"
 sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/World_Backup"
@@ -57,14 +59,9 @@ sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/restore_world.bash"
 sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/git_delete.bash"
 sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/check.sh"
 
-
-sudo bash -c 'for file in `find /opt/MC_Manage/ -maxdepth 1 -type f` ;do sed -i -e "s/\r//g" "$file" && echo $[++i].OK ;done'
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/download.sh"
 
 sudo systemctl daemon-reload
 
 
-sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/download.sh"
-
-
 exit 0
-
