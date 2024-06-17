a6b0f4bc99fe9867012c508ea9360a91cfa3357e
qiitaqiitaykuyo <QiitaQiitaYkuyo@outlook.jp>
2024-06-17 13:47:55 +0900

    Scripts/USER

--- Scripts/Update/update_2.sh
+++ Scripts/Update/update_2.sh
@@ -2,5 +2,6 @@
 updateDIR="$(dirname -- $(realpath -- "${BASH_SOURCE[0]:-$0}"))"
 echo "$updateDIR"
 echo "${updateDIR%/Scripts/Update}"
-git clone --depth 1 "https://github.com/qiitaqiitaykuyo/Minecraft-bedrock-server_Backup-by-Git..git" "${updateDIR%/Scripts/Update}"
-echo OK
+
+
+echo Updated.
