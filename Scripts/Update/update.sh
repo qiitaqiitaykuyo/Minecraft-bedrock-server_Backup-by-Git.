d4a48c552ff0e828dbf78a79733a2752993a80b1
qiitaqiitaykuyo <QiitaQiitaYkuyo@outlook.jp>
2024-06-17 14:48:43 +0900

    file

--- Scripts/Update/update.sh
+++ Scripts/Update/update.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 updateDIR="$(dirname -- $(realpath -- "${BASH_SOURCE[0]:-$0}"))"
 baseDIR="${updateDIR%/Scripts/Update}"
-parentDIR="$(dirname "$baseDIR")"
 
 find "$baseDIR" -xdev -depth ! \( -name . -or -name "update.sh" \) -exec rm -df {} +
 
 git clone --depth 1 "https://github.com/qiitaqiitaykuyo/Minecraft-bedrock-server_Backup-by-Git..git" "$baseDIR/.minecraft.git"
 
-\cp -Rf "$baseDIR/.minecraft.git" "$parentDIR"
+\cp -TRf "$baseDIR/.minecraft.git" "$baseDIR"
 
 find "$baseDIR" -type f -name "update_2.sh" -exec bash -- {} +
 
