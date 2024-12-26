653d88e5165b8753b4087dcc32f55986ab265606
qiitaqiitaykuyo <QiitaQiitaYkuyo@outlook.jp>
2024-12-26 20:53:20 +0900

    Changed

--- Scripts/Update/update_1.sh
+++ Scripts/Update/update_1.sh
@@ -20,5 +20,18 @@ sudo \cp -TRf "$baseDIR/Scripts/Minecraft/" "/opt/MC_Manage/Scripts/"
 sudo bash -c 'for file in `find "/opt/MC_Manage/Scripts/" -maxdepth 1 -type f` ;do sed -i -e "s/\r//g" "$file" ;done'
 sudo chown -R minecraft:minecraft "/opt/MC_Manage/Scripts/"
 
+### Create function file from bash script. ###
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/Properties"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/World_Backup"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/mc_env.bash"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/.bash_profile"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/variable.bash"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/mc_tmux.bash"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/nonstop_save.bash"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/backup_props.bash"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/backup_worlds.bash"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/restore_world.bash"
+sudo -i -u minecraft bash "/opt/MC_Manage/Scripts/git_delete.bash"
+
 find "$baseDIR" -type f -name "update_2.sh" -exec bash -- {} +
 
