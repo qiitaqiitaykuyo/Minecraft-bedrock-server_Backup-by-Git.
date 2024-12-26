43061a696c5363dad43bd84b03ba1721a33fbebf
qiitaqiitaykuyo <QiitaQiitaYkuyo@outlook.jp>
2024-12-26 18:19:18 +0900

    add

--- Scripts/Minecraft/download.sh
+++ Scripts/Minecraft/download.sh
@@ -2,7 +2,7 @@
 
 DOWNLOAD_URL=$(curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -s -L -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; BEDROCK-UPDATER)" https://minecraft.net/en-us/download/server/bedrock/ |  grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux/[^"]*')
 
-wget $DOWNLOAD_URL -O ~/bedrock-server.zip
+wget --no-check-certificate $DOWNLOAD_URL -O ~/bedrock-server.zip
 
 unzip -o bedrock*server*.zip
 
