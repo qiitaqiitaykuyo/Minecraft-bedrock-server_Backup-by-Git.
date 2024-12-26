492c7c4924403970cb22558203f9431e31c5aac6
qiitaqiitaykuyo <QiitaQiitaYkuyo@outlook.jp>
2024-12-26 17:48:00 +0900

    Change URL

--- Scripts/Minecraft/download.sh
+++ Scripts/Minecraft/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOWNLOAD_URL=$(curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -s -L -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; BEDROCK-UPDATER)" https://minecraft.net/en-us/download/server/bedrock/ |  grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*')
+DOWNLOAD_URL=$(curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -s -L -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; BEDROCK-UPDATER)" https://minecraft.net/en-us/download/server/bedrock/ |  grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux/[^"]*')
 
 wget $DOWNLOAD_URL -O ~/bedrock-server.zip
 
